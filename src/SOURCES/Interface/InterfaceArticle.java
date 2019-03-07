fcf8b467b7a164c374e867eb1c8dbdac781cecbd
Serge SULA <sulabosiog@gmail.com>
2019-03-07 21:37:55 +0100

    Initial commit

---
+++ src/SOURCES/Interface/InterfaceArticle.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SOURCES.Interface;
+
+/**
+ *
+ * @author HP Pavilion
+ */
+public interface InterfaceArticle {
+    
+    //Conatantes - BETA
+    public static final int BETA_EXISTANT = 0;
+    public static final int BETA_MODIFIE = 1;
+    public static final int BETA_NOUVEAU = 2;
+    
+    
+    public abstract int getId();
+    public abstract String getNom();
+    public abstract double getQte();
+    public abstract String getUnite();
+    public abstract int getIdMonnaie();
+    public abstract double getPrixUHT_avant_rabais();
+    public abstract double getRabais();
+    public abstract int getTranches();
+    public abstract double getPrixUHT_apres_rabais();
+    public abstract double getTvaPoucentage();
+    public abstract double getTvaMontant();
+    public abstract double getTotalTTC();
+    public abstract int getBeta();  // 0 = Existant, 1 =  Modifié, 2 = Nouveau
+    
+    public abstract void setId(int id);
+    public abstract void setNom(String nom);
+    public abstract void setQte(double Qt);
+    public abstract void setUnite(String unite);
+    public abstract void setIdMonnaie(int idMonnaie);
+    public abstract void setTranches(int tranches);
+    public abstract void setPrixUHT_avant_rabais(double prixUht);
+    public abstract void setRabais(double rabais);
+    public abstract void setTvaPoucentage(double tvapourc);
+    public abstract void setBeta(int newbeta);
+}
